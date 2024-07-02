54e95e3c85e442967a5dac24390b78c70ade8cf9
smallyou0 <whiteroot45811@gmail.com>
2024-07-02 11:26:57 +0900

    upload4

---
+++ js/common.js
@@ -0,0 +1,20 @@
+
+
+// $('.btn-area').click(function() {
+// $('.btn-area').removeClass('active');
+//     $(this).toggleClass('active');
+//         $('.layer').show();
+//         $('.modal-1').show();
+//     })
+$('.alert').click(function() {
+    $('.layer').show();
+    $('.modal-1').show();
+})
+$('#btnNo').click(function() {
+$('.layer').hide();
+$('.modal-1').hide();
+})
+// $('#btnYes').click(function() {
+// window.location.href = 'planner.html';
+// })
+  
